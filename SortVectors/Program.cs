db01b0430f864ccef0158edc91d5770f27779aa9
Francesco Tonelli <FRANCESCO.TONELLI@local.ittcesena.it>
2020-10-07 11:47:45 +0200

    Second step (Ordinamento)

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -11,9 +11,18 @@ namespace SortVectors
             for(int i = 0; i < vett.Length; i++)
             {
                 vett[i] = r.Next(-10, 11);
+            }
+            Ordina(vett);
+            for (int i = 0; i < vett.Length; i++)
+            {
                 Console.Write(vett[i] + " ");
             }
             Console.ReadKey();
+        }
+
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
         }
     }
 }
